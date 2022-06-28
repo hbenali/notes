4fa1e2fe9963aef88a2bb0cc849a4f2c59fa18ab
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-06-28 10:26:20 +0100

    MEED-347: Add ellipsis for note title in favorite drawer (#583)

--- notes-webapp/src/main/webapp/vue-app/notes-extensions/components/favorites/NotesFavoriteItem.vue
+++ notes-webapp/src/main/webapp/vue-app/notes-extensions/components/favorites/NotesFavoriteItem.vue
@@ -8,7 +8,7 @@
     </v-list-item-icon>
 
     <v-list-item-content>
-      <v-list-item-title class="text-color body-2">{{ noteTitle }}</v-list-item-title>
+      <v-list-item-title class="text-color body-2 text-truncate">{{ noteTitle }}</v-list-item-title>
     </v-list-item-content>
 
     <v-list-item-action>
